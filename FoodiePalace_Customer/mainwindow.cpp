e6c80dd04d830ec747ce4f631f0a4f88c6d86b4a
Creed667 <junaid121dark@gmail.com>
2022-01-05 20:39:57 +0600

    file read and write of users and order history done

--- FoodiePalace_Customer/mainwindow.cpp
+++ FoodiePalace_Customer/mainwindow.cpp
@@ -1,10 +1,132 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+QVector<User> usersInfoArray;
+class User userObject;
+
+QVector<Food> foodInfoArray;
+class Food foodObject;
+long long userTotalBill;
+
+void readUserFile()
+{
+      QString userName;
+      QString mobileNumber;
+      int userAge;
+      QString userGender;
+      QString password;
+     // QVector<history> userHistory;
+      QString temp1;
+      int historySize;
+      history tempHistory;
+
+      QFile file("D:\\Source code\\FoodiePalace_CustomerVersion\\FoodiePalace_Customer\\users.txt");
+
+      if (!file.open(QFile::ReadOnly | QFile::Text))
+      {
+        qDebug() << "Not Ok 2";
+        return;
+      }
+      QTextStream in(&file);
+      usersInfoArray.clear();
+      //base=0;
+      while (!in.atEnd())
+      {
+        userName = in.readLine();
+        temp1 = in.readLine();
+        userGender = in.readLine();
+        mobileNumber = in.readLine();
+        password = in.readLine();
+        userAge = temp1.split(" ")[0].toInt();
+        temp1 = in.readLine();
+        historySize = temp1.split(" ")[0].toInt();
+        for(int i=0;i<historySize;i++)
+        {
+        tempHistory.foodname  = in.readLine();
+        tempHistory.amount = in.readLine();
+        tempHistory.bill = in.readLine();
+        tempHistory.date = in.readLine();
+        userObject.userHistory.push_back(tempHistory);
+        }
+        userObject.setAllUserInfo(userName,userAge,userGender,mobileNumber,password);
+        usersInfoArray.push_back(userObject);
+
+      }
+}
+void saveUserFile()
+{
+    qDebug() <<"file writing";
+  QFile file("D:\\Source code\\FoodiePalace_CustomerVersion\\FoodiePalace_Customer\\users.txt");
+  QFile file2("D:\\Source code\\FoodiePalace_CustomerVersion\\FoodiePalace_Customer\\orderHistory.txt");
+
+  if (!file.open(QFile::WriteOnly | QFile::Text))
+  {
+    qDebug() << "Not ok";
+    return;
+  }
+  if (!file2.open(QFile::WriteOnly | QFile::Text))
+  {
+    qDebug() << "Not ok2";
+    return;
+  }
+
+  QTextStream out(&file);
+  QTextStream out2(&file2);
+  for (int i = 0; i < usersInfoArray.size(); i++)
+  {
+    QString temp1;
+    out << usersInfoArray[i].getUserName() << "\n";
+    temp1 = QString::number(usersInfoArray[i].getUserAge());
+    out << temp1 << "\n";
+    out << usersInfoArray[i].getUserGender() << "\n";
+    out << usersInfoArray[i].getMobileNumber() << "\n";
+    out << usersInfoArray[i].getPassword() << "\n";
+    out << usersInfoArray[i].userHistory.size() <<"\n";
+    for(int j=0;j< usersInfoArray[i].userHistory.size();j++)
+    {
+        out << usersInfoArray[i].userHistory[j].foodname<<"\n";
+        out << usersInfoArray[i].userHistory[j].amount<<"\n";
+        out << usersInfoArray[i].userHistory[j].bill<<"\n";
+        out << usersInfoArray[i].userHistory[j].date<<"\n";
+
+        //writing in orderHistory file which will be read in Owner version program
+        //this should be the common file between two version of programs
+        out2 << usersInfoArray[i].userHistory[j].foodname<<"\n";
+        out2 << usersInfoArray[i].userHistory[j].amount<<"\n";
+        out2 << usersInfoArray[i].userHistory[j].bill<<"\n";
+    }
+    }
+
+  }
+
+void showFile()
+{
+    for(int i=0;i<usersInfoArray.size();i++)
+    {
+    qDebug() <<usersInfoArray[i].getUserName();
+    qDebug() <<usersInfoArray[i].getMobileNumber();
+    qDebug() <<usersInfoArray[i].getPassword();
+    qDebug() <<usersInfoArray[i].getUserAge();
+    qDebug() <<usersInfoArray[i].getUserGender();
+    qDebug() <<usersInfoArray[i].userHistory.size();
+    for(int j=0;j<usersInfoArray[i].userHistory.size();j++)
+    {
+        qDebug() <<usersInfoArray[i].userHistory[j].foodname;
+        qDebug() <<usersInfoArray[i].userHistory[j].amount;
+        qDebug() <<usersInfoArray[i].userHistory[j].bill;
+        qDebug() <<usersInfoArray[i].userHistory[j].date;
+    }
+    }
+}
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    readUserFile();
+    showFile();
+    saveUserFile();
     ui->setupUi(this);
     ui->stackedWidget->setCurrentIndex(3);
 
--- FoodiePalace_Customer/mainwindow.h
+++ FoodiePalace_Customer/mainwindow.h
@@ -2,6 +2,26 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QString>
+#include <QtDebug>
+#include <QMessageBox>
+#include <iostream>
+#include <QDialogButtonBox>
+#include <QVector>
+#include <QDialog>
+#include <QInputDialog>
+#include <QDir>
+#include <QIcon>
+#include <QListWidgetItem>
+#include <QDockWidget>
+#include <QFont>
+#include <QSize>
+#include <QPicture>
+#include <QMap>
+#include <QPair>
+#include <QRegularExpression>
+#include <QRegularExpressionValidator>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -44,3 +64,119 @@ private:
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
+
+
+struct history{
+  QString foodname;
+  QString amount, bill;
+  QString date;
+};
+
+class Food
+{
+private:
+    QString foodName;
+    int foodPrice;
+    int foodStock;
+public:
+    void setAllFoodInfo(QString foodName,int foodPrice,int foodStock)
+    {
+        this->foodName=foodName;
+        this->foodPrice=foodPrice;
+        this->foodStock=foodStock;
+    }
+    QString getFoodName()
+    {
+        return foodName;
+    }
+    int getFoodPrice()
+    {
+        return foodPrice;
+    }
+    int getFoodStock()
+    {
+        return foodStock;
+    }
+    void setFoodName(QString newFoodName)
+    {
+        foodName=newFoodName;
+    }
+    void setFoodPrice(int newfoodPrice)
+    {
+        foodPrice=newfoodPrice;
+    }
+    void setFoodStock(int newFoodStock)
+    {
+        foodStock=newFoodStock;
+    }
+};
+
+class User
+{
+  private:
+    QString userName;
+    int userAge;
+    QString userGender;
+    QString mobileNumber;
+    QString password;
+  public:
+    QVector<history> userHistory;
+    void setAllUserInfo(QString userName,int userAge,QString userGender,QString mobileNumber,QString password)
+    {
+        this->userName=userName;
+        this->userAge=userAge;
+        this->userGender=userGender;
+        this->mobileNumber=mobileNumber;
+        this->password=password;
+
+    }
+
+    QString getUserName()
+    {
+        return userName;
+    }
+    QString getMobileNumber()
+    {
+        return mobileNumber;
+    }
+    QString getPassword()
+    {
+        return password;
+    }
+    int getUserAge()
+    {
+        return userAge;
+    }
+    QString getUserGender()
+    {
+        return userGender;
+    }
+    void setUserName(QString newName)
+    {
+        userName=newName;
+    }
+    void setMobileNumber(QString newMobileNumber)
+    {
+        mobileNumber=newMobileNumber;
+    }
+    void setPassword(QString newPassword)
+    {
+        qDebug()<<"new pass"<<newPassword;
+        password=newPassword;
+    }
+    void setUserAge(int age)
+    {
+        userAge=age;
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
